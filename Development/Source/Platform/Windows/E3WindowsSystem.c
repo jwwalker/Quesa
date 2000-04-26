016f7ba96b03fc2c04c8169dbf029d74aaa951f0
pepe <pepe@b7bc8a59-ec99-4242-a7c6-685af75001c2>
2000-04-26 15:49:15 +0000

    Added plug-in support

--- Development/Source/Platform/Windows/E3WindowsSystem.c
+++ Development/Source/Platform/Windows/E3WindowsSystem.c
@@ -37,8 +37,118 @@
 #include "E3System.h"
 
 
+//=============================================================================
+//      Private types
+//-----------------------------------------------------------------------------
+typedef struct E3WindowsSystem_DLLSlot *E3WindowsSystem_DLLSlotPtr;
+
+typedef struct E3WindowsSystem_DLLSlot{
+
+	HMODULE						Dll;
+	E3WindowsSystem_DLLSlotPtr	nextSlot;
+
+}E3WindowsSystem_DLLSlot;
 
 
+
+//=============================================================================
+//      Private global data
+//-----------------------------------------------------------------------------
+
+
+E3WindowsSystem_DLLSlot* e3windowssystem_dllSlotHead = NULL;
+
+
+//=============================================================================
+//      Private functions
+//-----------------------------------------------------------------------------
+//      e3windowsystem_unloadplugins : Closes and unloads the plug-ins.
+//-----------------------------------------------------------------------------
+static void
+e3windowsystem_unloadplugins()
+{
+
+		E3WindowsSystem_DLLSlot* nextSlot;
+		E3WindowsSystem_DLLSlot* currentSlot;
+
+		nextSlot = e3windowssystem_dllSlotHead;
+
+		while( nextSlot != NULL){
+			currentSlot = nextSlot;
+			nextSlot = currentSlot->nextSlot;
+			FreeLibrary(currentSlot->Dll);
+			E3Memory_Free(&currentSlot);
+		}
+
+}
+
+
+//=============================================================================
+//      e3windowsystem_load1plugin : Opens and memorizes the DLL pointed by path.
+//-----------------------------------------------------------------------------
+static E3WindowsSystem_DLLSlot*
+e3windowsystem_load1plugin(LPTSTR path)
+{
+	E3WindowsSystem_DLLSlot* newSlot = NULL;
+
+
+
+	newSlot = (E3WindowsSystem_DLLSlot*)E3Memory_Allocate(sizeof(E3WindowsSystem_DLLSlot));
+	if(newSlot != NULL){
+		newSlot->Dll = LoadLibrary(path);
+		if(newSlot->Dll != NULL){
+			newSlot->nextSlot = e3windowssystem_dllSlotHead;
+			e3windowssystem_dllSlotHead = newSlot;
+		}
+		else{//LoadLibrary failed
+			E3Memory_Free(&newSlot);
+		}
+	}
+
+	return newSlot;
+
+}
+
+
+//=============================================================================
+//      e3windowsystem_loadplugins : Scans the directory dir 
+//										and loads files with extension ext.
+//-----------------------------------------------------------------------------
+static void
+e3windowsystem_loadplugins(LPTSTR dir, LPTSTR ext)
+{
+
+	WIN32_FIND_DATA FindFileData; 
+	HANDLE hFindFile; 
+	TCHAR szFiles[MAX_PATH]; 
+	TCHAR szFileName[MAX_PATH]; 
+	E3WindowsSystem_DLLSlot* lastLoadedModule;
+ 
+	lstrcpy(szFiles, dir); 
+	lstrcat(szFiles, TEXT("\\*.")); 
+	lstrcat(szFiles, ext); 
+ 
+
+	hFindFile = FindFirstFile(szFiles, &FindFileData); 
+ 
+	if (hFindFile == INVALID_HANDLE_VALUE) { 
+		return; 
+	} 
+ 
+
+	
+	do { 
+		lstrcpy(szFileName, dir); 
+		lstrcat(szFileName, TEXT("\\")); 
+		lstrcat(szFileName, FindFileData.cFileName); 
+		
+		lastLoadedModule = e3windowsystem_load1plugin(szFileName);
+
+	} while (FindNextFile(hFindFile, &FindFileData) 
+						|| (GetLastError() != ERROR_NO_MORE_FILES)); 
+ 
+	FindClose(hFindFile); 
+}
 
 //=============================================================================
 //      Public functions
@@ -67,7 +177,7 @@ void
 E3WindowsSystem_Terminate(void)
 {
 
-
+	e3windowsystem_unloadplugins();
 	// Terminate the system
 }
 
@@ -84,4 +194,12 @@ E3WindowsSystem_Terminate(void)
 void
 E3WindowsSystem_LoadPlugins(void)
 {
+	TCHAR systemDir[MAX_PATH];
+
+	if(GetSystemDirectory(systemDir, MAX_PATH * sizeof(*systemDir)) > 0)
+		e3windowsystem_loadplugins(systemDir,TEXT("xq3"));
+
+	if(GetWindowsDirectory(systemDir, MAX_PATH * sizeof(*systemDir)) > 0)
+		e3windowsystem_loadplugins(systemDir,TEXT("xq3"));
+	
 }
