80fc426e17714c24e2e1c3a9827bd4af8d7e9a5c
James Walker <jamesw@frameforge3d.com>
2001-12-03 21:14:37 +0000

    Implemented __sys_pointer_size, use NewPtr under Mac OS X, added comments.

--- Development/Source/Platform/Mac/E3MacMemory.c
+++ Development/Source/Platform/Mac/E3MacMemory.c
@@ -37,10 +37,42 @@
 
 #include <Memory.h>
 #include <Types.h>
+#include <Gestalt.h>
 
 #include <pool_alloc.h>
 
-
+/*
+	============================================================================
+	Purpose
+	
+	When building Quesa as a shared library, we want Quesa to be able to allocate
+	memory from the global heap rather than sharing the application's memory
+	partition.  When Quesa is a static library, it will use the same malloc as
+	the application.
+	
+	When running under Mac OS X, application memory partitions don't exist, so
+	we can use NewPtr instead of TempNewHandle.  Besides being more efficient,
+	this makes it easier to debug with MallocDebug.
+	
+	
+	MSL notes
+	
+	If you are debugging, or building for Mac OS X only, you may want to rebuild
+	the MSL C library with the macro _MSL_OS_DIRECT_MALLOC defined.  This
+	basically makes malloc call __sys_alloc directly, instead of suballocating
+	from big pools allocated with __sys_alloc.  If you use this option, be
+	warned that as of CodeWarrior Pro 7, there is a bug in the implementation of
+	realloc.  If you call realloc( ptr, 0 ), it fails to call free( ptr ).
+	
+	If you use the usual pooled malloc configuration, you may want to comment
+	out the line
+		#define _MSL_USE_FIX_MALLOC_POOLS
+	in alloc.c.  In my experience, that makes memory allocation behave more
+	predictably.
+	
+	-- JWWalker 3 Dec 2001
+	============================================================================
+*/
 
 
 
@@ -60,8 +92,25 @@
 // Alignment - see Tech Note 1174
 #define AlignPtrToBoundary(_rawPtr, _align)		(Ptr) ((((TQ3Uns32) _rawPtr)+(_align)-1) & ~((_align)-1))
 
-
-
+//=============================================================================
+//      IsMemoryPartitioned : Determine whether the Mac memory allocator
+//			pays attention to the memory partition in the SIZE resource.
+//-----------------------------------------------------------------------------
+#if !QUESA_BUILD_AS_STATIC_LIBRARY
+static Boolean IsMemoryPartitioned()
+{
+	static Boolean	sDidCheck = false;
+	static Boolean	sIsPartitioned = true;
+	if (! sDidCheck)
+	{
+		long	response;
+		Gestalt( gestaltSystemVersion, &response );
+		sIsPartitioned = (response < 0x1000);
+		sDidCheck = true;
+	}
+	return sIsPartitioned;
+}
+#endif
 
 
 //=============================================================================
@@ -73,63 +122,89 @@
 
 void *
 __sys_alloc(__std(size_t) theSize)
-{   TQ3Uns32    totalMem, memAvail, requiredSize;
-    Handle      theHnd;
-    Ptr			thePtr;
-    OSErr       theErr;
+{
+	Ptr			thePtr;
+	
+	if (IsMemoryPartitioned())
+		{
+		TQ3Uns32    totalMem, memAvail, requiredSize;
+	    Handle      theHnd;
+	    OSErr       theErr;
 
 
 
-    // Check our parameters
-    if (theSize == 0)
-        return(NULL);
+	    // Check our parameters
+	    if (theSize == 0)
+	        return(NULL);
 
 
 
-    // Work out how much memory we need.
-    requiredSize = theSize + sizeof(Handle) + kBlockAlignmentSize;
+	    // Work out how much memory we need.
+	    requiredSize = theSize + sizeof(Handle) + kBlockAlignmentSize;
 
 
 
-    // Try and allocate some memory from temp mem first.
-    // If that fails, we compact the app heap and try there.
-    theHnd = TempNewHandle(requiredSize, &theErr);
-    if (theHnd == NULL)
-        {
-        PurgeSpace((SInt32 *) &totalMem, (SInt32 *) &memAvail);
-        if (requiredSize + kAppHeapThreshold <= memAvail)
-            {
-            PurgeMem(requiredSize);       
-            theHnd = NewHandle(requiredSize);
-            }
-        }
+	    // Try and allocate some memory from temp mem first.
+	    // If that fails, we compact the app heap and try there.
+	    theHnd = TempNewHandle(requiredSize, &theErr);
+	    if (theHnd == NULL)
+	        {
+	        PurgeSpace((SInt32 *) &totalMem, (SInt32 *) &memAvail);
+	        if (requiredSize + kAppHeapThreshold <= memAvail)
+	            {
+	            PurgeMem(requiredSize);       
+	            theHnd = NewHandle(requiredSize);
+	            }
+	        }
 
 
 
-    // If we didn't get the handle, we're done.
-    if (theHnd == NULL)
-        return(NULL);
+	    // If we didn't get the handle, we're done.
+	    if (theHnd == NULL)
+	        return(NULL);
 
 
 
-	// Offset the pointer in past the first four bytes, offset it some more to
-	// make sure we return an optimally aligned block, then write the handle
-	// four bytes behind the pointer we return.
-    MoveHHi(theHnd);
-    HLock(theHnd);
+		// Offset the pointer in past the first four bytes, offset it some more to
+		// make sure we return an optimally aligned block, then write the handle
+		// four bytes behind the pointer we return.
+	    MoveHHi(theHnd);
+	    HLock(theHnd);
 
-	thePtr = (*theHnd) + 4;
-	thePtr = AlignPtrToBoundary(thePtr, 32);
+		thePtr = (*theHnd) + 4;
+		thePtr = AlignPtrToBoundary(thePtr, kBlockAlignmentSize);
 
-	*((Handle *) (thePtr - 4)) = theHnd;
+		*((Handle *) (thePtr - 4)) = theHnd;
+		}
+	else
+		{
+		thePtr = NewPtr( theSize );
+		}
 
     return(thePtr);
 }
 
 #endif // QUESA_BUILD_AS_STATIC_LIBRARY
 
-
-
+//=============================================================================
+//      __sys_pointer_size : Free a block of memory.
+//-----------------------------------------------------------------------------
+//		MSL uses this when compiled with the _MSL_OS_DIRECT_MALLOC option.
+//		Note : We only want to override MSL when building as a shared library.
+//-----------------------------------------------------------------------------
+#if !QUESA_BUILD_AS_STATIC_LIBRARY
+__std(size_t) __sys_pointer_size(void* ptr)
+{
+	if (IsMemoryPartitioned())
+		{
+		Ptr	thePtr = (Ptr)ptr;
+		Handle	theHandle = *((Handle *) (thePtr - 4));
+		return GetHandleSize( theHandle ) - sizeof(Handle) - kBlockAlignmentSize;
+		}
+	else
+		return GetPtrSize( (Ptr)ptr );
+}
+#endif // QUESA_BUILD_AS_STATIC_LIBRARY
 
 
 //=============================================================================
@@ -141,21 +216,24 @@ __sys_alloc(__std(size_t) theSize)
 
 void
 __sys_free(void *thePtr)
-{   Handle      theHnd, *hndPtr;
-
-
-
+{
     // Chec our parameters
     if (thePtr == NULL)
         return;
 
-
-
-    // Recover the handle from the pointer and free the block.
-    hndPtr = (Handle *) thePtr;
-    theHnd = *(hndPtr - 1);
-    
-    DisposeHandle(theHnd);
+	if (IsMemoryPartitioned())
+		{
+		Handle      theHnd, *hndPtr;
+	    // Recover the handle from the pointer and free the block.
+	    hndPtr = (Handle *) thePtr;
+	    theHnd = *(hndPtr - 1);
+	    
+	    DisposeHandle(theHnd);
+		}
+	else
+		DisposePtr( (Ptr) thePtr );
+		
+	Q3_ASSERT( noErr == MemError() );
 }
 
 #endif // QUESA_BUILD_AS_STATIC_LIBRARY
