37aba1c03ddedcd0854bf9f8e6c78e33074ea5f2
dair <dair@b7bc8a59-ec99-4242-a7c6-685af75001c2>
2002-03-29 19:44:19 +0000

    Updated copyright date.

--- Development/Source/Platform/Cocoa/E3CocoaDrawContextNotify.m
+++ Development/Source/Platform/Cocoa/E3CocoaDrawContextNotify.m
@@ -13,7 +13,7 @@
         events for the application.
 
     COPYRIGHT:
-        Quesa Copyright © 1999-2001, Quesa Developers.
+        Quesa Copyright © 1999-2002, Quesa Developers.
         
         For the list of Quesa Developers, and contact details, see:
         
