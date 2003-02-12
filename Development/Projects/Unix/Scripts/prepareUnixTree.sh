6b7fce6de11c475fcb6fff33ca0f3b9722d4e758
pepe <pepe@b7bc8a59-ec99-4242-a7c6-685af75001c2>
2003-02-12 20:16:07 +0000

    Added support to build examples

--- Development/Projects/Unix/Scripts/prepareUnixTree.sh
+++ Development/Projects/Unix/Scripts/prepareUnixTree.sh
@@ -14,16 +14,31 @@
 
 pushd ../../Unix || exit
 
-mkdir Source
 
 ln -sf ../../../SDK/Includes/Quesa APIincludes
 
+mkdir Source
 pushd Source
 
 ln -sf ../../../Source/Core
 ln -sf ../../../Source/FileFormats
 ln -sf ../../../Source/PlatForm/Unix
 ln -sf ../../../Source/Renderers
+
+popd
+
+mkdir Examples
+pushd Examples
+
+ln -sf ../acinclude.m4
+ln -sf ../ltmain.sh
+ln -sf ../configure.in.examples configure.in
+ln -sf ../Makefile.am.examples Makefile.am
+
+
+ln -sf ../../../../SDK/Examples/Qut
+ln -sf "../../../../SDK/Examples/Geom Test/Geom Test.c" GeomTest.c
+ln -sf "../../../../SDK/Examples/Import Test/Import Test.c" ImportTest.c
 
 popd
 
