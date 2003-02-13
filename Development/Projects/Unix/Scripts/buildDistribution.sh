e99d9a0eb024bfc97be75d11e36a65d81394ec70
pepe <pepe@b7bc8a59-ec99-4242-a7c6-685af75001c2>
2003-02-13 00:26:39 +0000

    added notes

--- Development/Projects/Unix/Scripts/buildDistribution.sh
+++ Development/Projects/Unix/Scripts/buildDistribution.sh
@@ -11,6 +11,20 @@
 #
 # Be sure to be on the /Development/Projects/Unix/Scripts directory before run this
 #
+#
+# things that could fail:
+#	aclocal complains because the undefined macro AM_PATH_GTK
+#		if gtk is installed search a file called gtk.m4 and pass its directory
+#			to aclocal as parameter es.
+#					 aclocal -I /usr/local/share/aclocal
+#
+#
+#	configure complains because doesn't find gtk-config
+#			search gtk-config and add its parent directory to your PATH environment variable
+#
+#
+
+
 
 pushd ../../Unix || exit
 
