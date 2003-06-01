cf46d59e78ace0bbdc82d354c23c4baae0a3521e
dair <dair@b7bc8a59-ec99-4242-a7c6-685af75001c2>
2003-06-01 13:22:34 +0000

    Renamed:   kQ3WarningTriMeshTriangleNotNormalized   kQ3WarningTriMeshVertexNotNormalized
    to:
      kQ3WarningTriangleNotNormalized
      kQ3WarningVertexNotNormalized
    
    Since not specific to TriMesh geometries.

--- Development/Source/Core/System/E3Errors.c
+++ Development/Source/Core/System/E3Errors.c
@@ -259,8 +259,8 @@ static TQ3StringIdentifier gEnglishStringTable_Warning[] = {
 	{ kQ3WarningInvalidObjectInGroupMetafile,		"Quesa Warning: " "Invalid object in group metafile" },
 	{ kQ3WarningQuaternionNotNormalized,			"Quesa Warning: " "Quaternion is not normalized" },
 	{ kQ3WarningNormalCanNotBeFlipped,				"Quesa Warning: " "Normal can not be flipped correctly" },
-	{ kQ3WarningTriMeshTriangleNotNormalized,		"Quesa Warning: " "TriMesh triangle normal is normalized" },
-	{ kQ3WarningTriMeshVertexNotNormalized,			"Quesa Warning: " "TriMesh vertex normal is not normalized" },
+	{ kQ3WarningTriangleNotNormalized,				"Quesa Warning: " "Triangle normal is not normalized" },
+	{ kQ3WarningVertexNotNormalized,				"Quesa Warning: " "Vertex normal is not normalized" },
 	{ kQ3WarningReadInfiniteFloatingPointNumber,	"Quesa Warning: " "Read infinite floating-point number" },
 	{ kQ3WarningReadBadNormalVector,				"Quesa Warning: " "Read bad (unnormalizable) normal vector" },
 	{ kQ3WarningNone, NULL } };
