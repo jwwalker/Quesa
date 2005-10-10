d5ef5e2ea563657986b9266e0565665a230d7d96
James Walker <jamesw@frameforge3d.com>
2005-10-10 22:00:01 +0000

    Up to 1.0.3.

--- SDK/Extras/VRML Reader/Resources/version.h
+++ SDK/Extras/VRML Reader/Resources/version.h
@@ -42,7 +42,7 @@
 */
 #define		kNumVersionMajor	1
 #define		kNumVersionMinor	0
-#define		kNumVersionBugfix	2
+#define		kNumVersionBugfix	3
 
-#define		kStringVersion		"1.0.2"
-#define		kUnquotedVersion	1.0.2
+#define		kStringVersion		"1.0.3"
+#define		kUnquotedVersion	1.0.3
