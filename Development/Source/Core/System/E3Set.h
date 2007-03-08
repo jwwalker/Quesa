b0fcedda4f31bac46bd448d3d1542478383120f0
James Walker <jamesw@frameforge3d.com>
2007-03-08 02:15:52 +0000

    Fixed spelling of transparencyColor member of TQ3SetAttributes.  Added emissiveColor to TQ3SetAttributes.

--- Development/Source/Core/System/E3Set.h
+++ Development/Source/Core/System/E3Set.h
@@ -5,7 +5,7 @@
         Header file for E3Set.c.
 
     COPYRIGHT:
-        Copyright (c) 1999-2004, Quesa Developers. All rights reserved.
+        Copyright (c) 1999-2007, Quesa Developers. All rights reserved.
 
         For the current release of Quesa, please see:
 
@@ -122,7 +122,8 @@ typedef struct TQ3SetAttributes {
 	float				specularControl;
 	TQ3Param2D 			shadingUV;
 	TQ3SurfaceShaderObject surfaceShader;
-	TQ3ColorRGB			trasparencyColor;
+	TQ3ColorRGB			transparencyColor;
+	TQ3ColorRGB			emissiveColor;
 } TQ3SetAttributes;
 
 
