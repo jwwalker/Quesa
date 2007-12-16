96ea30dfb8f3260c29135fdb2eed4c6cde1a4252
jwwalker <jwwalker@b7bc8a59-ec99-4242-a7c6-685af75001c2>
2007-12-16 02:58:25 +0000

    Support full-screen antialiasing option.

--- SDK/Extras/Cocoa Example/Quesa3DView/AppDelegate.h
+++ SDK/Extras/Cocoa Example/Quesa3DView/AppDelegate.h
@@ -5,7 +5,7 @@
         Header file for AppDelegate.m.
 
     COPYRIGHT:
-        Copyright (c) 1999-2004, Quesa Developers. All rights reserved.
+        Copyright (c) 1999-2007, Quesa Developers. All rights reserved.
 
         For the current release of Quesa, please see:
 
@@ -53,6 +53,7 @@
 - (void)setZRot:(id)sender;
 - (void)setGeometryFromTag:(id)sender;
 - (void)toggleAnimation:(id)sender;
+- (void)toggleAntialiasing:(id)sender;
 - (void)setRendererFromTag:(id)sender;
 - (void)toggleBoundingBox:(id)sender;
 @end
