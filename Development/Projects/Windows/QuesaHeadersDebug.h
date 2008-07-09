c8b0ccdf1271409aad9023664a0bbd4089d64e04
James Walker <jamesw@frameforge3d.com>
2008-07-09 23:44:55 +0000

    Updated for Borland compatibility.

--- Development/Projects/Windows/QuesaHeadersDebug.h
+++ Development/Projects/Windows/QuesaHeadersDebug.h
@@ -1,9 +1,24 @@
-
-#ifdef __cplusplus
-	#include <Win32Headers++.mch>
-#else
-	#include <Win32Headers.mch>
+#ifdef __MWERKS__
+	#ifdef __cplusplus
+		#include <Win32Headers++.mch>
+	#else
+		#include <Win32Headers.mch>
+	#endif
 #endif
+
+#ifdef __BORLANDC__
+	#define WIN32                     		1
+	#define _WINDOWS
+	#define _USRDLL;
+	#define QD3D_NO_DIRECTDRAW
+
+	#include <math.h>
+	#define isfinite _finite
+	#define fabsf(_n) (float)fabs((double)(_n))
+#endif
+
+#define QUESA_OS_WIN32            		1
+#define QUESA_ALLOW_QD3D_EXTENSIONS		1
 
 #define Q3_EXPORT_SYMBOLS 1
 #define QUESA_EXPORTS 1
