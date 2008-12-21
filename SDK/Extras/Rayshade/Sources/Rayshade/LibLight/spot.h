527c4de871be9ed6a71a7bd7337aef270142dd90
James Walker <jamesw@frameforge3d.com>
2008-12-21 02:04:29 +0000

    Spot light improvements from Roger Holmes.

--- SDK/Extras/Rayshade/Sources/Rayshade/LibLight/spot.h
+++ SDK/Extras/Rayshade/Sources/Rayshade/LibLight/spot.h
@@ -13,9 +13,12 @@
  * There is no warranty or other guarantee of fitness of this software
  * for any purpose.  It is provided solely "as is".
  *
- * $Id: spot.h,v 1.1 2002-12-18 18:36:42 pepe Exp $
+ * $Id: spot.h,v 1.2 2008-12-21 02:04:29 jwwalker Exp $
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2002/12/18 18:36:42  pepe
+ * First upload
+ *
  * Revision 4.0  91/07/17  14:35:50  kolb
  * Initial version.
  * 
@@ -23,15 +26,21 @@
 #ifndef SPOT_H
 #define SPOT_H
 
-#define LightSpotCreate(c,f,t,p,i,o) LightCreate( \
-			(LightRef)SpotCreate(f,t,p,i,o), SpotMethods(), c)
+#define LightSpotCreate(c,f,t,i,o,at,fo) LightCreate( \
+			(LightRef)SpotCreate(f,t,i,o,at,fo), SpotMethods(), c)
 typedef struct {
 	Vector pos, dir;
-	Float coef, radius, falloff;
+	Float hotAngle ;
+	Float outerAngle ;
+	Float cosHotAngle ;
+	Float cosOuterAngle ;
+	int	attenuation ;
+	int	fallOff ;
 } Spotlight;
 
 extern Spotlight *
-SpotCreate(Vector *from,Vector *to,Float coef,Float in,Float out);
+SpotCreate ( Vector* from , Vector* dir , Float hotAngle , Float  
+			outerAngle , int attenuation , int fallOff ) ;
 extern LightMethods *SpotMethods(void);
                 
 
